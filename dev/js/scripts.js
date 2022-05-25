fd3c0b1a23af001cc436eaacc41730730a5daca9
EricSweeney <ejs363@drexel.edu>
2022-05-25 18:43:11 -0400

    Animation

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,7 +6,7 @@ import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
 gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, MorphSVGPlugin);
 
-const mainTL = gsap.timeline();
+const mainTL = gsap.timeline({delay: 0.5});
 
 mainTL
 //.from("#Ring-4", {duration: 0.4, transformOrigin: '50% 50%', scale: 0, opacity: 0, ease: "back.out(1)"})
@@ -30,95 +30,143 @@ function loadingIn(){
     return tl;
 }
 
-function orbitIn(){
-    let tl = gsap.timeline({repeat:-1});
+function loadingScale(){
+    let tl = gsap.timeline();
 
     tl
-    //.to(".FullPlanet", {duration: 1, ease: "power1.out", x: 500}, "one")
-    //.to(".FullPlanet", {duration: 1, ease: "power1.in", x: 0}, "two")
-    //.to(".FullPlanet", {duration: 1, ease: "power1.out", x: -500}, "three")
-    //.to(".FullPlanet", {duration: 1, ease: "power1.in", x: 0}, "four")
-
-    .to("#FullPlanet", {duration: 1, ease: "power1.out", x: 200}, "one")
-    .to("#FullPlanet", {duration: 1, ease: "power1.in", x: 0}, "two")
-    .to("#FullPlanet", {duration: 1, ease: "power1.out", x: -200}, "three")
-    .to("#FullPlanet", {duration: 1, ease: "power1.in", x: 0}, "four")
-
+    .from("#LoadingText", {duration: 1, scale: 0, transformOrigin: '50% 50%', opacity: 0, y: 200, ease: "back.out"})
     ;
 
     return tl;
 }
 
-function lineFollow(){
-    let tl = gsap.timeline({repeat:-1});
+function planetScale(){
+    let tl = gsap.timeline();
 
     tl
-
-    .from("#Rectangle-right", {duration: 1, scaleX: 0, ease: "power1.out"}, "one")
-    .to("#Rectangle-right", {duration: 1, scaleX: 0, ease: "power1.in"}, "two")
-    .from("#Rectangle-left", {duration: 1, scaleX: 0, transformOrigin: '100%', ease: "power1.out"}, "three")
-    .to("#Rectangle-left", {duration: 1, scaleX: 0, transformOrigin: '100%', ease: "power1.in"}, "four")
+    .from("#Sun", {duration: 0.75, scale: 0, transformOrigin: '50% 50%', opacity: 0, ease: "back.out"})
+    .from("#SunOrbit", {duration: 0.5, opacity: 0, ease: "back.out"}, "<50%")
+    .from("#SunOrbitTwo", {duration: 0.5, opacity: 0, ease: "back.out"}, "<50%")
+    .from("#SunOrbitThree", {duration: 0.5, opacity: 0, ease: "back.out"}, "<50%")
+    .from("#Planet", {duration: 0.75, scale: 0, transformOrigin: '50% 50%', opacity: 0, ease: "back.out"}, "<50%")
+    .from("#PlanetTwo", {duration: 0.75, scale: 0, transformOrigin: '50% 50%', opacity: 0, ease: "back.out"}, "<50%")
+    .from("#PlanetThree", {duration: 0.75, scale: 0, transformOrigin: '50% 50%', opacity: 0, ease: "back.out"}, "<50%")
+    .from("#Orbit", {duration: 0.5, opacity: 0, ease: "back.out"}, "<50%")
+    .from("#Moon", {duration: 0.75, scale: 0, transformOrigin: '50% 50%', opacity: 0, ease: "back.out"}, "<50%")
+    .from("#OrbitTwo", {duration: 0.5, opacity: 0, ease: "back.out"}, "<50%")
+    .from("#OrbitTwoTwo", {duration: 0.5, opacity: 0, ease: "back.out"}, "<50%")
+    .from("#MoonTwo", {duration: 0.75, scale: 0, transformOrigin: '50% 50%', opacity: 0, ease: "back.out"}, "<50%")
+    .from("#MoonTwoTwo", {duration: 0.75, scale: 0, transformOrigin: '50% 50%', opacity: 0, ease: "back.out"}, "<50%")
+    .from("#OrbitThree", {duration: 0.5, opacity: 0, ease: "back.out"}, "<50%")
+    .from("#OrbitThreeThree", {duration: 0.5, opacity: 0, ease: "back.out"}, "<50%")
+    .from("#MoonThree", {duration: 0.75, scale: 0, transformOrigin: '50% 50%', opacity: 0, ease: "back.out"}, "<50%")
+    .from("#MoonThreeThree", {duration: 0.75, scale: 0, transformOrigin: '50% 50%', opacity: 0, ease: "back.out"}, "<50%")
     ;
 
     return tl;
 }
-
-//function moonFirstOrbit(){
-    //let tl = gsap.timeline();
-
-    //tl
-    //.to(".FullPlanet", {duration: 2, ease:"none", rotation: "180_cw", transformOrigin: "0% 0%"})
-    //.to("#Moon", {duration: 1, ease:"none", motionPath:{path:"#Orbit", align: "#Orbit", start:0.25, end:1, alignOrigin:[0.5, 0.5]}})
-    //;
-
-    //return tl;
-
-//}
 
 function moonOrbit(){
     let tl = gsap.timeline({repeat:-1});
 
     tl
-    //.to(".FullPlanet", {duration: 2, ease:"none", rotation: "180_cw", transformOrigin: "0% 0%"})
-    //.to("#Moon", {duration: 1, ease:"none", motionPath:{path:"#Orbit", align: "#Orbit", start: 0.5, end:1, alignOrigin:[0.5, 0.5]}})
-    .to("#Moon", {duration: 3, ease:"none", motionPath:{path:"#Orbit", align: "#Orbit", start: 0, end:1, alignOrigin:[0.5, 0.5]}})
+    .to("#Moon", {duration: 2, ease:"none", motionPath:{path:"#Orbit", align: "#Orbit", start: 0, end:1, alignOrigin:[0.5, 0.5]}}, "begin")
+    
     ;
 
     return tl;
 
 }
 
+function moonOrbitTwo(){
+    let tl = gsap.timeline({repeat:-1});
 
-//transformOrigin: '50% 50%', scale: 0.6,
+    tl
+    .to("#MoonTwo", {duration: 2, ease:"none", motionPath:{path:"#OrbitTwo", align: "#OrbitTwo", start: 0, end:1, alignOrigin:[0.5, 0.5]}}, "begin")
+    ;
 
+    return tl;
+
+}
+
+function moonOrbitTwoTwo(){
+    let tl = gsap.timeline({repeat:-1});
+
+    tl
+    .to("#MoonTwoTwo", {duration: 3, ease:"none", motionPath:{path:"#OrbitTwoTwo", align: "#OrbitTwoTwo", start: 0, end:1, alignOrigin:[0.5, 0.5]}}, "begin")
+    ;
+
+    return tl;
+
+}
+
+function moonOrbitThree(){
+    let tl = gsap.timeline({repeat:-1});
+
+    tl
+    .to("#MoonThree", {duration: 3, ease:"none", motionPath:{path:"#OrbitThree", align: "#OrbitThree", start: 0, end:1, alignOrigin:[0.5, 0.5]}}, "begin")
+    ;
+
+    return tl;
+
+}
+
+function moonOrbitThreeThree(){
+    let tl = gsap.timeline({repeat:-1});
+
+    tl
+    .to("#MoonThreeThree", {duration: 4, ease:"none", motionPath:{path:"#OrbitThreeThree", align: "#OrbitThreeThree", start: 0, end:1, alignOrigin:[0.5, 0.5]}}, "begin")
+    ;
+
+    return tl;
+
+}
+
+function sunOrbit(){
+    let tl = gsap.timeline({repeat:-1});
+
+    tl
+    //.to("SunOrbit", {duration: 4, drawSVG:"0% 100%"})
+    .to("#FullPlanet", {duration: 4, ease:"none", motionPath:{path:"#SunOrbit", align: "#SunOrbit", start: 0, end:1, alignOrigin:[0.5, 0.5]}}, "begin")
+    ;
+
+    return tl;
+
+}
+
+function sunOrbitTwo(){
+    let tl = gsap.timeline({repeat:-1});
+
+    tl
+    .to("#FullPlanetTwo", {duration: 7, ease:"none", motionPath:{path:"#SunOrbitTwo", align: "#SunOrbitTwo", start: 0, end:1, alignOrigin:[0.5, 0.5]}}, "begin")
+    ;
+
+    return tl;
+
+}
+
+function sunOrbitThree(){
+    let tl = gsap.timeline({repeat:-1});
+
+    tl
+    .to("#FullPlanetThree", {duration: 9, ease:"none", motionPath:{path:"#SunOrbitThree", align: "#SunOrbitThree", start: 0, end:1, alignOrigin:[0.5, 0.5]}}, "begin")
+    ;
+
+    return tl;
+
+}
 
 mainTL
-.add(moonOrbit())
-.to("#Orbit", {duration: 0.0001, opacity: 0})
-.from("#LoadingText", {duration: 3, opacity: 0, ease: "elastic.out(0.5, 0.3)", y: 200}, "begin")
-.add(loadingIn())
-.from("#FullPlanet", {duration: 3, opacity: 0, ease: "elastic.out(0.5, 0.7)", y: -450}, "begin")
-.to("#FullPlanet", {duration: 2, ease: "power1.inOut", x: -200}, "start1")
-.to("#FullPlanet", {duration: 1, ease: "power1.in", x: 0}, "start2")
-//.from("#Rectangle-left", {duration: 2, scaleX: 0, transformOrigin: '100%', ease: "power1.inOut"}, "start1")
-//.to("#Rectangle-left", {duration: 1, scaleX: 0, transformOrigin: '100%', ease: "power1.in"}, "start2")
-.add(orbitIn(), "true")
-.add(lineFollow(), "true")
+.add(loadingScale(), "go")
+.add(planetScale(), "go")
+.add(loadingIn(), "go")
+.add(moonOrbit(), "go")
+.add(moonOrbitTwo(), "go")
+.add(moonOrbitTwoTwo(), "go")
+.add(moonOrbitThree(), "go")
+.add(moonOrbitThreeThree(), "go")
+.add(sunOrbit(), "go")
+.add(sunOrbitTwo(), "go")
+.add(sunOrbitThree(), "go")
 
 
-
-//add delay
-//figure out how to change motionpath origin
-//why is it rotating twice around path
-
-//.to("#Planet-1", {duration: 7, ease:"power2.in", motionPath:{path:"#Ring-1", align: "#Ring-1", alignOrigin:[0.5, 0.5]}}, "orbit")
-;
-
-//let secTL = gsap.timeline({repeat: -1});
-
-//secTL
-//.to("#Planet-1", {duration: 6, ease:"none", motionPath:{path:"#Ring-1", align: "#Ring-1", alignOrigin:[0.5, 0.5]}}, 1)
-//.to("#Planet-2", {duration: 4, ease:"none", motionPath:{path:"#Ring-2", align: "#Ring-2", alignOrigin:[0.5, 0.5]}}, 1)
-//.to("#Planet-3", {duration: 3, ease:"none", motionPath:{path:"#Ring-3", align: "#Ring-3", alignOrigin:[0.5, 0.5]}}, 1)
-//.to("#Planet-4", {duration: 2, ease:"none", motionPath:{path:"#Ring-4", align: "#Ring-4", alignOrigin:[0.5, 0.5]}}, 1)
-//;
